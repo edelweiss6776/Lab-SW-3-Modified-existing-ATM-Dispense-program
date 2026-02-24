3573752d0586bab347145fa76f02022b6cb1b56d
Jairus Joshua Ramos <ramosjairus6776@gmail.com>
2026-02-24 11:29:54 +0800

    Refactor ATMDispenseChain initialization for dispensers

--- ATMDispenseChain.java
+++ ATMDispenseChain.java
@@ -3,19 +3,19 @@ public class ATMDispenseChain implements DispenseChain {
     public ATMDispenseChain() {
         // Initialize the chain
         this.nextChain = new Peso1000Dispenser();
-        DispenseChain c2 = new Peso500Dispenser();
-        DispenseChain c3 = new Peso200Dispenser();
-        DispenseChain c4 = new Peso100Dispenser();
-        DispenseChain c5 = new Peso50Dispenser();
-        DispenseChain c6 = new Peso20Dispenser();
+        DispenseChain peso500Dispenser = new Peso500Dispenser();
+        DispenseChain peso200Dispenser = new Peso200Dispenser();
+        DispenseChain peso100Dispenser = new Peso100Dispenser();
+        DispenseChain peso50Dispenser = new Peso50Dispenser();
+        DispenseChain peso20Dispenser = new Peso20Dispenser();
 
 
         // Set the chain of responsibility
-        nextChain.setNextChain(c2);
-        c2.setNextChain(c3);
-        c3.setNextChain(c4);
-        c4.setNextChain(c5);
-        c5.setNextChain(c6);
+        nextChain.setNextChain(peso500Dispenser);
+        peso500Dispenser.setNextChain(peso200Dispenser);
+        peso200Dispenser.setNextChain(peso100Dispenser);
+        peso100Dispenser.setNextChain(peso50Dispenser);
+        peso50Dispenser.setNextChain(peso20Dispenser);
     }
 
 
